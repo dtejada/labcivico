2ef36638c44938687e0bdc24a415e8bae0038e29
Catalina Margozzini <ca.margozzini@gmail.com>
2015-03-31 17:54:07 -0300

    Nuevo home

--- js/main.js
+++ js/main.js
@@ -64,18 +64,20 @@
 
         // Sticky Menu
         MenuSticky: function () {
-            var menu = document.querySelector('#menu'),
-                origOffsetY = menu.offsetTop + 100;
-            function scroll() {
-                if ($(window).scrollTop() >= origOffsetY) {
-                    $('#menu').addClass('sticky');
-                    $('#menu').removeClass('fixed');
-                } else {
-                    $('#menu').removeClass('sticky');
-                    $('#menu').addClass('fixed');
+            if (document.getElementById('menu')) {
+                var menu = document.querySelector('#menu'),
+                    origOffsetY = menu.offsetTop + 100;
+                function scroll() {
+                    if ($(window).scrollTop() >= origOffsetY) {
+                        $('#menu').addClass('sticky');
+                        $('#menu').removeClass('fixed');
+                    } else {
+                        $('#menu').removeClass('sticky');
+                        $('#menu').addClass('fixed');
+                    }
                 }
+                document.onscroll = scroll;
             }
-            document.onscroll = scroll;
         },
 
 
